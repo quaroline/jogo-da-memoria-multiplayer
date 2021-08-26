06f4ca4eecf95cd00e829489289f1b632dc0421e
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-08-26 13:38:59 -0300

    Aplicação para testar tecnologias de back-end antes de iniciar o desenvolvimento completo.

---
+++ server.js
@@ -0,0 +1,33 @@
+const { SSL_OP_NO_TICKET } = require('constants')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+let mensagens = []
+
+io.on('connection', socket => {
+    console.log('Socket conectado ' + socket.id)
+
+    socket.emit('previousMessages', mensagens)
+
+    socket.on('sendMessage', dados => {
+        mensagens.push(dados)
+
+        socket.broadcast.emit('receivedMessage', dados)
+    })
+})
+
+server.listen(3000)
