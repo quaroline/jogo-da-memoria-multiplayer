c8a02c16c1f7943ed958cb4afffa6688bc889d77
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-08-28 20:53:14 -0300

    Design dos quadrinhos finalizado.

--- app/src/app/quadrinho/quadrinho.component.ts
+++ app/src/app/quadrinho/quadrinho.component.ts
@@ -1,16 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TipoPecinha } from '../models/tipo-pecinha';
 
 @Component({
   selector: 'app-quadrinho',
   templateUrl: './quadrinho.component.html',
-  styleUrls: ['./quadrinho.component.css']
+  styleUrls: ['./quadrinho.component.scss'],
+  animations: [
+    trigger('cardFlip', [
+      state('default', style({
+        transform: 'none',
+      })),
+      state('flipped', style({
+        transform: 'perspective(600px) rotateY(180deg)'
+      })),
+      state('matched', style({
+        visibility: 'false',
+        transform: 'scale(0.05)',
+        opacity: 0
+      })),
+      transition('default => flipped', [
+        animate('400ms')
+      ]),
+      transition('flipped => default', [
+        animate('400ms')
+      ]),
+      transition('* => matched', [
+        animate('400ms')
+      ])
+    ])
+  ]
 })
 export class QuadrinhoComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {}
 
-  @Input()
-  public urlImagem: string = '';
+  @Input() 
+  public pecinha: TipoPecinha;
+
+  @Output() 
+  public quadroClicado = new EventEmitter();
 }
