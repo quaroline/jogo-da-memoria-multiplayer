31dc46fe91b2730481838215708cdfd617a20ec9
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-08-28 20:52:05 -0300

    Alteração de componentes.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppComponent } from './app.component';
 import { QuadrinhoComponent } from './quadrinho/quadrinho.component';
 
@@ -10,7 +12,10 @@ import { QuadrinhoComponent } from './quadrinho/quadrinho.component';
     QuadrinhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+	  ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
