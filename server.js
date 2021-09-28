0160d2a5443fba6a3c52cb394f89754fff03e0eb
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-09-28 05:19:36 -0300

    Retirado ";".

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
 const express = require('express')
-const cors = require('cors')
-const { v4: uuidv4 } = require('uuid')
+
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const cors = require('cors')
+
+const { v4: uuidv4 } = require('uuid')
+
 app.use(cors({
     credentials: true, 
     origin: true}
-));
+))
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
 
 app.get('/api/novo-jogo', (_, res) => {
-    res.json(uuidv4());
+    res.json(uuidv4())
 })
 
 app.get('/', (_, res) => {
-    res.send('Rota inicial funcionando.');
+    res.send('Rota inicial funcionando.')
 })
 
 io.on('connection', (socket) => {
     console.log('Socket conectado ' + socket.id)
-
-    socket.emit('previousMessages', mensagens)
-
-    socket.on('sendMessage', dados => {
-        mensagens.push(dados)
-
-        socket.broadcast.emit('receivedMessage', dados)
-    })
 })
 
 server.listen(3000)
