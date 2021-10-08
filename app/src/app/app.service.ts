baed53a5802ca9d1166184026a9f412d0d8c9bc2
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-10-08 17:04:48 -0300

    Criados métodos na service.

--- app/src/app/app.service.ts
+++ app/src/app/app.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+import { Jogador } from './models/jogador';
+import { Partida } from './models/partida';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,39 @@ export class AppService {
 
   constructor(private socket: Socket) { }
 
-  iniciarNovoJogo() {
+  private observer: any;
+
+  pegarJogador(): Observable<Jogador> {
+    this.socket.on('pegarJogador', (jogador: Jogador) => {
+      this.observer.next(jogador);
+    });
+
+    return this.getObservable();
+  }
+
+  iniciarNovoJogo(): Observable<Partida> {
     this.socket.emit('novo-jogo');
+
+    this.socket.on('pegarPartida', (partida: Partida) => {
+      this.observer.next(partida);
+    });
+
+    return this.getObservable();
+  }
+
+  entrarEmPartidaExistente(partidaId: string): Observable<Partida> {
+    this.socket.emit('entrar-em-partida-existente', partidaId);
+
+    this.socket.on('pegarPartida', (partida: Partida) => {
+      this.observer.next(partida);
+    });
+
+    return this.getObservable();
+  }
+
+  private getObservable(): Observable<any> {
+    return new Observable(observer => {
+      this.observer = observer;
+    });
   }
 }
