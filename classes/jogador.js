f321b3cb17285a618323e62fa759cbec0d0e2124
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-10-08 17:03:26 -0300

    (Backend) Criação de jogadores.

---
+++ classes/jogador.js
@@ -0,0 +1,5 @@
+class Jogador {
+    
+}
+
+module.exports = Jogador
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+'use strict';
+
 const express = require('express')
+const Jogador = require('./classes/jogador')
+const Utils = require('./classes/utils')
+
+const jogador = new Jogador()
+const utils = new Utils()
 
 const app = express()
 const server = require('http').createServer(app)
@@ -10,12 +17,39 @@ const io = require('socket.io')(server, {
 
 const { v4: uuidv4 } = require('uuid')
 
-io.on('connection', (socket) => {
-    console.log('Socket conectado ' + socket.id)
+const jogadores = []
 
+const entrarEmPartida = (socket, partidaId) => {
+    socket.join(partidaId)
+
+    utils.enviarInformacoesParaCliente(socket, 'pegarInformacoesDaPartida', { 
+        partidaId, 
+        jogadorId: socket.id 
+    })
+}
+
+const inicializarJogador = (socket) => {
+    let jogador = { 
+        id: socket.id, 
+        nomeDeUsuario: utils.gerarNomeDeUsuario() 
+    }
+
+    jogadores.push(jogador)
+
+    utils.enviarInformacoesParaCliente(socket, 'pegarJogador', jogador)
+}
+
+io.on('connection', (socket) => {
+    inicializarJogador(socket)
+
+    // Rotas
     socket.on("novo-jogo", () => {
-        console.log(uuidv4())
+        entrarEmPartida(socket, `partida_${uuidv4()}`)
     });
+
+    socket.on("entrar-em-partida-existente", (partidaId) => {
+        entrarEmPartida(socket, `partida_${partidaId}`)
+    })
 })
 
 server.listen(3000)
